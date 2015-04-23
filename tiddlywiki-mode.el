0790067ca19ca0a5239bc348b8ca02472c4807bf
natto <natto@natto-mba.local>
2015-04-23 08:11:09 -0400

    add init modefile with prelim .tid parser

---
+++ tiddlywiki-mode.el
@@ -0,0 +1,16 @@
+(defun tiddlywiki-parse-tid-file ()
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (let ((prop-list nil)
+          (content nil)
+          (in-header t))
+      (while in-header
+        (if (looking-at "^\\([^:]+\\):[[:space:]]+?\\(.+\\)$")
+            ;; in header
+            (setq prop-list (plist-put prop-list (match-string 1) (match-string 2)))
+          ;; end read header
+          (setq in-header nil))
+        (forward-line))
+      (setq prop-list (plist-put prop-list :content (buffer-substring (point) (point-max))))
+      prop-list)))
