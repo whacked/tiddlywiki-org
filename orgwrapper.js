53d6a5bdb092db2a29a4293c3323a0351551030d
natto <natto@natto-tpy.none>
2015-12-17 12:38:56 -0500

    add preprocessor for transclusion, prepend "#" for TW link

--- orgwrapper.js
+++ orgwrapper.js
@@ -14,8 +14,35 @@ Wraps up the org-mode-parser parser for use in TiddlyWiki5
 var Org = require("$:/plugins/whacked/org-js/org.js");
 
 exports["text/org"] = function(type,text,options) {
+
+  // preprocess:
+  // - pre-apply transclusion directives:
+  //   #+INCLUDE: "source_file.org" :lines N-M
+  //   for now we will just inject it completely
+  // - change local_link to #local_link for TW intralinking
+  var includePattern = /\s*#\+INCLUDE:\s*"?([^ "]+)"?\s*(?::lines\s+(\d+)[-~](\d+))?\s*?/;
+  
+  var arrPreproc = [];
+  text.split("\n").forEach(function(origLine) {
+    var procLine;
+    
+    var transcludeMatch = origLine.match(includePattern);
+    if(Array.isArray(transcludeMatch)) {
+      var sourceFile = transcludeMatch[1],
+          lineBegin = transcludeMatch[2], // ignored for now
+          lineEnd = transcludeMatch[3], // ignored for now
+          sourceTiddler = sourceFile.replace(/\.[^\.]+$/, ""); // strip extension
+      procLine = options.wiki.getTextReference(sourceTiddler, "ERROR: no text", null);
+    } else {
+      // note the "#" --> this is needed for TW's href for local tiddlers
+      // (otherwise the link will be seen as e.g. http://localhost:8080/link
+      procLine = origLine.replace(/\[\[(.+?)\.tid\](\[.+?\])?\]/g, "[[#$1]$2]");
+    }
+    arrPreproc.push(procLine);
+  });
+  
   var orgParser = new Org.Parser();
-  var orgDocument = orgParser.parse(text);
+  var orgDocument = orgParser.parse(arrPreproc.join("\n"));
   var orgHTMLDocument = orgDocument.convert(Org.ConverterHTML, {
     headerOffset: 0,
     exportFromLineNumber: false,
