e5a6c9924928a71255ba65a287e79e36c30e5364
natto <natto@natto-tpy.none>
2016-03-18 14:20:45 -0400

    add handler for direct json input to executable js type

--- executable-javascript.js
+++ executable-javascript.js
@@ -60,35 +60,78 @@ try {
     return out;
   }
 
+  function ensure_renderable(data, text) {
+    var rtn;
+    if(Array.isArray(data)) {
+      rtn = data;
+    } else {
+      rtn = [data];
+    }
+    // weak check;
+    // a stronger check will check from renderable types
+    if(rtn[0].type) {
+      return rtn;
+    } else {
+      return [{
+        type: "codeblock",
+        attributes: {
+          code: {type: "string", value: text || JSON.stringify(data)},
+          language: {type: "string", value: "javascript"}
+        }
+      }];
+    }
+  }
+
 exports["application/js"] = function(type,text,options) {
   /**
      COMMENTARY
 
      this function calls eval() on the input javascript, which is
-     expected to be a function definition that takes a single
-     argument, `env`, which contains custom information that your
-     function might be interested in. Right now it only loads the text
-     of other tiddlers; see `parse_header`.
+     expected to be either:
+
+     - a direct tiddler object description. This will be attempted
+     first by trying to parse the tiddler contents as JSON. If
+     successful, AND it contains the 'type' and 'attributes' keys,
+     it is returned directly. Example:
+
+     #+BEGIN_SRC javascript
+     {
+       type: "codeblock",
+       attributes":
+       {
+         "code": {"type": "string", "value": "var x = 1;"},
+         "language": {"type": "string", "value": "javascript"}
+       }
+     }
+     #+END_SRC
+
+     if the special keys are absent, it will be wrapped into and
+     rendered as a JSON codeblock.
+     
+     - a function definition that takes a single argument, `env`,
+     which contains custom information that your function might be
+     interested in. Right now it only loads the text of other
+     tiddlers; see `parse_header`.
 
      what the input arguments do:
      
      - `type` will be "application/js" (what the plugin exports)
      - `text` will be the actual text content of the tiddler that uses
-        this content-type (i.e. selected "application/js" from the
-        dropdown)
+     this content-type (i.e. selected "application/js" from the
+     dropdown)
      - `options` is TiddlyWiki internal. Don't know much about it,
-        although it is used to load external tiddler references via
-        transclusion. More notes on transclusion later below.
+     although it is used to load external tiddler references via
+     transclusion. More notes on transclusion later below.
      
      this plugin expects your function to return one of:
      
      - a `String`, which will be sent as raw HTML content of the tiddler
      - an `Array`, whose children are assumed to be widget specs.
      - an `Object`, which is assumed to be a single tiddlywiki widget
-       specification. No sanity checking is done. The object gets
-       wrapped into an `Array`.
+     specification. No sanity checking is done. The object gets
+     wrapped into an `Array`.
      
-     syntax: org-mode
+     (below notes in org-mode syntax)
      
      NOTE:
      
@@ -102,7 +145,7 @@ exports["application/js"] = function(type,text,options) {
      Also see http://tiddlywiki.com/dev/static/Parser.html
 
      It looks like the lowest level type is the "string" type;
-     i.e. you cannot set return a raw string; the raw string must be
+     i.e. you cannot just return a raw string; the raw string must be
      wrapped in a ={type: "string", value: $raw_string}=. Then, a
      =text= type is a text DOM node.
      
@@ -114,13 +157,13 @@ exports["application/js"] = function(type,text,options) {
      
      #+BEGIN_SRC javascript
      this.tree = [{type: "element",
-                   tag: "h1",
-                   attributes: {
-                     "style": {type: "string", value: "border:2px solid blue;"}
-                   },
-                   children: [
-                     {type: "text", text: "Hello, I am the header text!"}
-                   ]}];
+     tag: "h1",
+     attributes: {
+     "style": {type: "string", value: "border:2px solid blue;"}
+     },
+     children: [
+     {type: "text", text: "Hello, I am the header text!"}
+     ]}];
      #+END_SRC
 
      Translusion at render phase
@@ -129,19 +172,19 @@ exports["application/js"] = function(type,text,options) {
      
      #+BEGIN_SRC javascript
      var transcludeNode = {
-       type: "transclude",
-       attributes: {tiddler: {type: "string",
-                              value: "NAME_OF_SOURCE_TIDDLER"}}
+     type: "transclude",
+     attributes: {tiddler: {type: "string",
+     value: "NAME_OF_SOURCE_TIDDLER"}}
      };
      this.tree = [transcludeNode]; // this does the single node transclusion
      
      // Another usage:
      this.tree = [{
-       type: "tiddler",
-       attributes: {
-         tiddler: {type: "string", value: targetTitle}
-	   	 },
-	   	 children: [transcludeNode]
+     type: "tiddler",
+     attributes: {
+     tiddler: {type: "string", value: targetTitle}
+	   },
+	   children: [transcludeNode]
      }];
      #+END_SRC
 
@@ -153,6 +196,14 @@ exports["application/js"] = function(type,text,options) {
      =console.log(options.wiki.getTextReference("my_test_tiddler", "ERROR: no text", null));=
   */
   var parsed = parse_header(text);
+  try {
+    var json = JSON.parse(parsed.body);
+    this.tree = ensure_renderable(json, parsed.body);
+    return;
+  } catch(e) {
+    // assume the tiddler is a function expression
+  }
+
   var env = {};
   Object.keys(parsed.env).forEach(function(varname) {
     env[varname] = options.wiki.getTextReference(parsed.env[varname], "ERROR: no text", null);
@@ -169,11 +220,7 @@ exports["application/js"] = function(type,text,options) {
   }
   switch(typeof result) {
   case "object":
-    if(Array.isArray(result)) {
-      this.tree = result;
-    } else {
-      this.tree = [result];
-    }
+    this.tree = ensure_renderable(result);
     break;
   default:
   case "string":
