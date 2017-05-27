9fb1afa69bbcc7327f827803d041395cdfca784a
natto <natto@natto-tpy.none>
2017-05-27 13:15:24 -0700

    cleanup junk code

--- Tiddler.py
+++ Tiddler.py
@@ -1,12 +1,16 @@
-import sys
-import os
-from os.path import exists as pexists, join as pjoin
-from glob import glob
 import datetime
-import email_parser as emparse
-reload(emparse)
+import os
+import sys
+from glob import glob
+from os.path import exists as pexists
+from os.path import join as pjoin
+from dateutil.parser import parse as parsedate
+from textwrap import dedent
+
 
 class Tiddler:
+
+    TIME_FORMAT = '%Y%m%d%H%M%S000'
 
     tiddlers_dir = None
 
@@ -16,9 +20,55 @@ class Tiddler:
         with open(pjoin(self.tiddlers_dir, self.title + '.tid'), 'w') as ofile:
             ofile.write(unicode(self))
 
-    def __init__(self, title, ):
+    def _parse(self, raw_tiddler):
+        '''
+        :type raw_tiddler: str
+        :return: None
+        '''
+        DIVIDER = '\n\n'
+        idx_header_end = raw_tiddler.index(DIVIDER)
+        header_lines = raw_tiddler[:idx_header_end]
+        to_parse = {
+            'created': False,
+            'modified': False,
+            'tags': False,
+            'title': False,
+            'type': False,
+        }
+        for line_ in header_lines:
+            line = line_.strip()
+            for field_name in to_parse:
+                entry_marker = '%s:'%field_name
+                if not line.startswith(entry_marker):
+                    continue
+                data = line.split(None, 1)[-1]
+                if entry_marker in ('created', 'modified'):
+                    setattr(self, entry_marker, parsedate(data))
+                elif entry_marker == 'tags':
+                    self.tags = [tag.strip() for tag in data.split(',')]
+                elif entry_marker == 'title':
+                    self.title = data
+                elif entry_marker == 'type':
+                    self.type = data
+        self.content = raw_tiddler[idx_header_end+2:].decode('utf-8')
+
+    def read_string(self, s):
+        self._parse(s)
+
+    def read(self, file_path):
+        with open(file_path) as ifile:
+            raw_tiddler = ifile.read()
+            self.read_string(raw_tiddler)
+
+    @classmethod
+    def load_from_file(cls, file_path):
+        t = cls(None)
+        t.read(file_path)
+        return t
+
+    def __init__(self, title, content=None, content_type=None):
         self.title = title
-        self.text = ''
+        self.content = content or ''
         self.creator = None
         self.modifier = None
         self.created = datetime.datetime.now()
@@ -27,7 +77,7 @@ class Tiddler:
         self.linksUpdated = False
         self.tags = []
         self.fields = {}
-        self.type = 'text/vnd.tiddlywiki'
+        self.type = content_type or 'text/vnd.tiddlywiki'
 
     def add_tag(self, tag):
         if ' ' in tag:
@@ -36,129 +86,18 @@ class Tiddler:
             self.tags.append(tag)
 
     def __unicode__(self):
-        return '''\
-created: %s
-modified: %s
-tags: %s
-title: %s
-type: %s
+        return dedent(u'''\
+            created: %s
+            modified: %s
+            tags: %s
+            title: %s
+            type: %s
 
-%s''' % (
-    self.created.strftime('%Y%m%d%H%M%S000'),
-    self.modified.strftime('%Y%m%d%H%M%S000'),
-    ' '.join(self.tags),
-    self.title,
-    self.type,
-    self.text,
-)
-    
-if __name__ == '__main__':
-    Tiddler.tiddlers_dir = 'tw/tiddlers'
-    tid = Tiddler('hello world again')
-    # print unicode(tid)
-    # tid.save()
-
-    from termcolor import colored
-    import mailbox
-    import html2text as h2t
-    from jwzthreading import thread, make_message
-
-    def to_text_list(msg):
-        rtn = []
-        if msg.is_multipart():
-            for part in msg.get_payload():
-                rtn.extend(to_text_list(part))
-                break
-        else:
-            rtn.append(msg.get_payload())
-        return rtn
-
-    class Store:
-        count = 0
-
-    org_out = []
-    SHOULD_OUTPUT_TIDDLER = False
-    def print_container(ctr, depth=0, parent=None):
-        import sys
-        sys.stdout.write(depth*' ')
-        if ctr.message:
-            sys.stdout.write(colored('%s -- %s' % (
-                ctr.message.subject,
-                ctr.message.message.get('from'),
-            ), 'yellow'))
-
-            org_out.append('%s %s %s' % (
-                '*'*(1+depth),
-                ctr.message.subject,
-                ctr.message.message.get('from'),
-            ))
-
-            tiddler_name = '%s (%s)' % (
-                ctr.message.subject.replace(':', '_'),
-                Store.count,
+            %s''') % (
+                self.created.strftime(self.TIME_FORMAT),
+                self.modified.strftime(self.TIME_FORMAT),
+                ' '.join(self.tags),
+                self.title,
+                self.type,
+                self.content,
             )
-            Store.count += 1
-
-            if SHOULD_OUTPUT_TIDDLER:
-                tid = Tiddler(tiddler_name)
-                if parent:
-                    tid.text = 'in response to: {{%s}}\n\n' % (parent)
-                else:
-                    tid.text = ''
-                tid.text += '\n'.join(to_text_list(ctr.message.message))
-                # tid.type = 'text/x-markdown'
-                tid.save()
-                print '\n>>> saved: %s' % tid.title
-
-            # orgmode text output
-            if True:
-                print
-                for n, ltext in enumerate(emparse.Parser.parse('\n'.join(to_text_list(ctr.message.message))), start=1):
-                    # print ltext
-                    if ltext.depth == 0:
-                        org_out.append(ltext.text)
-                    elif True:
-                        pass
-                    else:
-                        org_out.append('''\
-#+BEGIN_EXAMPLE
-%s
-#+END_EXAMPLE
-''' % ltext.text)
-                print n, 'lines processed.'
-
-
-            sys.stdout.write('\n')
-            # for text in to_text_list(ctr.message.message):
-            #     # print h2t.html2text(text.replace('\r\n', '\n').replace('=\n', ''))
-            #     print text
-        # if raw_input().strip() == 'q': return
-        for c in ctr.children:
-            print_container(c, depth+1, tiddler_name)
-
-    print('Reading input file...')
-
-    if False:
-        mbox_path = '/Volumes/ramdisk/ccmt6.mbox'
-    # with open(mbox_path, 'rb') as ifile:
-        # mbox = mailbox.UnixMailbox(ifile)
-        mbox = mailbox.mbox(mbox_path)
-        mlist = list(mbox)
-        msglist = [make_message(m) for m in mlist]
-    else:
-        eml_path = '/tmp/ccmt-1'
-        mlist = [mailbox.mboxMessage(open(eml).read()) for eml in glob(pjoin(eml_path, '*.eml'))]
-        msglist = [make_message(m) for m in mlist]
-
-    print('Threading...')
-    subject_table = thread(msglist)
-
-    # Output
-    L = subject_table.items()
-    L.sort()
-    for subj, container in L:
-        print_container(container)
-
-    with open('ccmt-1.org', 'w') as ofile:
-        ofile.write('\n'.join(org_out))
-        print 'ok:',ofile.name
